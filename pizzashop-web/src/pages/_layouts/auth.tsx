73728dedd29bd3048a30f27b86f4caf2be6fab27
murilogtss <murilogtss123@gmail.com>
2024-08-06 19:18:50 -0300

    feat: add initial sign-in page

--- pizzashop-web/src/pages/_layouts/auth.tsx
+++ pizzashop-web/src/pages/_layouts/auth.tsx
@@ -1,12 +1,23 @@
+import { PizzaIcon } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-    return ( <div>
-        <h1>Auth Layout</h1>
-        <div>
-            <Outlet />
+    return (
+        <div className="min-h-screen grid grid-cols-2">
+            <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+                <div className="flex items-center gap-2 text-lg font-medium text-foreground">
+                    <PizzaIcon size={20} />
+                    <span className="font-semibold">Pizza Shop</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do Parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+                </footer>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <Outlet />
+            </div>
         </div>
-    </div> );
+    );
 }
- 
+
 export default AuthLayout;
--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 
 const SignIn = () => {
     return (
         <>
             <Helmet title="Sign In" />
-            <h1>Sign In</h1>
+            <div className="p-8">
+                <div className="w-[350px] flex flex-col justify-center gap-6">
+                    <div className="flex flex-col gap-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">Acessar Painel</h1>
+                        <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do Parceiro</p>
+                    </div>
+
+                    <form className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Seu e-mail</Label>
+                            <Input id="email" type="email" />
+                        </div>
+                        <Button type="submit" className="w-full">Acessar Painel</Button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
