ea012a0725433a0082baa47a7ba5cf36462e0c84
murilogtss <murilogtss123@gmail.com>
2024-08-05 23:15:10 -0300

    chore: add react router dom setup

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { router } from './routes'
 
 function App() {
 
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
