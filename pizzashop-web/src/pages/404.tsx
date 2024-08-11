4e67bbe2b12700c90efc17d1fe1a3d520d668d82
Murilo Leite <murilogtss123@gmail.com>
2024-08-10 22:07:43 -0300

    feat: add 404 Not Found page

---
+++ pizzashop-web/src/pages/404.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return ( 
+        <div className="flex h-screen flex-col items-center justify-center gap-2">
+            <h1 className="text-4xl font-bold">Página não encontrada</h1>
+            <p className="text-accent-foreground">
+                Voltar para o <Link to='/' className="text-sky-500 dark:text-sky-400">Dashboard</Link> 
+            </p>
+        </div>
+     );
+}
+ 
+export default NotFound;
--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -5,16 +5,22 @@ import AuthLayout from "./pages/_layouts/auth";
 import SignUp from "./pages/auth/sign-up";
 import Orders from "./pages/app/orders/orders";
 import Dashboard from "./pages/app/dashboard/dashboard";
+import NotFound from "./pages/404";
 
 export const router = createBrowserRouter([
     {
-        path: '/', element: <AppLayout />, children: [
+        path: '/', 
+        element: <AppLayout />, 
+        errorElement: <NotFound />,
+        children: [
             { path: '/', element: <Dashboard /> },
             { path: '/orders', element: <Orders /> }
         ]
     },
     {
-        path: '/', element: <AuthLayout />, children: [
+        path: '/', 
+        element: <AuthLayout />, 
+        children: [
             { path: '/sign-in', element: <SignIn /> },
             { path: '/sign-up', element: <SignUp /> }
         ]
