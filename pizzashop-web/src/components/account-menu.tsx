b4d24967bbd0656003e6e9329eeb2e396ad71175
murilogtss <murilogtss123@gmail.com>
2024-08-12 21:45:56 -0300

    feat: ensure to show user info in the account-menu component of the header.

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -1,20 +1,34 @@
 import { BuildingIcon, ChevronDownIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 const AccountMenu = () => {
+
+    const { data: profile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile,
+    })
+
+    const { data: managedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant,
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant='outline' className="gap-2 select-none">
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDownIcon size={16} />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Murilo Leite</span>
-                    <span className="text-xs font-normal text-muted-foreground">murilogtss123@gmail.com</span>
+                    <span>{profile?.name}</span>
+                    <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
