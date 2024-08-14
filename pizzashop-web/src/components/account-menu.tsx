2320b0601413c5de7ca3fb0cafe98b66486154a8
murilogtss <murilogtss123@gmail.com>
2024-08-13 21:28:42 -0300

    feat: add store-profile-dialog component

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import StoreProfileDialog from "./store-profile-dialog";
 
 const AccountMenu = () => {
 
@@ -19,38 +21,45 @@ const AccountMenu = () => {
     })
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant='outline' className="gap-2 select-none">
-                    {isLoadingManagedRestaurant ? <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
-                    <ChevronDownIcon size={16} />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    {isLoadingProfile ? (
-                        <div className="space-y-1.5">
-                            <Skeleton className="h-4 w-24" />
-                            <Skeleton className="h-4 w-32" />
-                        </div>
-                    ) : (
-                        <>
-                            <span>{profile?.name}</span>
-                            <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
-                        </>
-                    )}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <BuildingIcon size={16} className="mr-2" />
-                    <span>Perfil da Loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOutIcon size={16} className="mr-2" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant='outline' className="gap-2 select-none">
+                        {isLoadingManagedRestaurant ? <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
+                        <ChevronDownIcon size={16} />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-24" />
+                                <Skeleton className="h-4 w-32" />
+                            </div>
+                        ) : (
+                            <>
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                            </>
+                        )}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <BuildingIcon size={16} className="mr-2" />
+                            <span>Perfil da Loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+                        <LogOutIcon size={16} className="mr-2" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+
+            <StoreProfileDialog />
+        </Dialog>
     );
 }
 
